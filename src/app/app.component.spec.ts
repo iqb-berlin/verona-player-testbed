27fef51f9deef23dbcf6e353fbbced06529b2ea5
rhenck <richard.henck@tu-berlin.de>
2020-10-20 12:43:01 +0200

    Add/Improve tests for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,36 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ]
+      imports: [RouterTestingModule],
+      declarations: [AppComponent]
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('should create the app', async () => {
+    expect(component).toBeTruthy();
   });
 
-  it('should have as title \'verona-player-testbed\'', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('verona-player-testbed');
+  it('should have as title \'IQB Verona Player Testbed\'', () => {
+    expect(component.title).toEqual('IQB Verona Player Testbed');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('verona-player-testbed app is running!');
+    expect(compiled.querySelector('.title').textContent).toContain('IQB Verona Player Testbed');
+  });
+
+  it('should create focus listeners', () => {
+    spyOn(window, 'addEventListener');
+    component.ngOnInit();
+    expect(window.addEventListener).toHaveBeenCalled();
   });
 });
