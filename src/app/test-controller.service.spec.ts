eb94968232b9c9c9be721794366d3bc85bf437d3
rhenck <richard.henck@tu-berlin.de>
2020-10-21 16:37:33 +0200

    Add tests for test-controller service

---
+++ src/app/test-controller.service.spec.ts
@@ -0,0 +1,66 @@
+import { TestBed } from '@angular/core/testing';
+import { ElementRef } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TestControllerService } from './test-controller.service';
+import { UploadFileType } from './test-controller.interfaces';
+
+fdescribe('TestControllerService', () => {
+  let service: TestControllerService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [TestControllerService]
+    });
+    service = TestBed.inject(TestControllerService);
+  });
+
+  it('should init variables empty', () => {
+    expect(service.playerName).toBe('');
+    expect(service.playerSourceCode).toBe('');
+    expect(service.unitList).toEqual([]);
+  });
+
+  it('should update presentation status', () => {
+    expect(service.status.presentation.color).toBe('Teal');
+    service.setPresentationStatus('complete');
+    expect(service.status.presentation.color).toBe('LimeGreen');
+  });
+
+  it('should update responses status', () => {
+    expect(service.status.responses.color).toBe('Teal');
+    service.setResponsesStatus('no');
+    expect(service.status.responses.color).toBe('Red');
+  });
+
+  /**
+   * Faking input element and corresponding change event.
+   * First set the upload file type, then send event.
+   */
+  it('should upload file', () => {
+    service.fileSelectElement = new ElementRef(document.createElement('input'));
+    service.setUploadFileRequest(UploadFileType.UNIT);
+
+    const event = {
+      target: {
+        files: [new File([new Blob()], 'dummy_filename')]
+      }
+    };
+    service.uploadFile(event as unknown as InputEvent);
+    expect(service.unitList.length).toBeGreaterThan(0);
+    expect(service.unitList[0].filename).toBe('dummy_filename');
+  });
+
+  it('should upload player', () => {
+    service.fileSelectElement = new ElementRef(document.createElement('input'));
+    service.setUploadFileRequest(UploadFileType.PLAYER);
+
+    const event = {
+      target: {
+        files: [new File([new Blob()], 'dummy_playername')]
+      }
+    };
+    service.uploadFile(event as unknown as InputEvent);
+    expect(service.playerName).toBe('dummy_playername');
+  });
+});
