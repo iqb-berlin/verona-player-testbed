8b0e705972e786885839efa5fb6336a99e4c6662
rhenck <richard.henck@iqb.hu-berlin.de>
2020-12-16 15:30:15 +0100

    Add unit tests for app-component
    Mainly listener tests with spies.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,36 +1,70 @@
-import { TestBed, ComponentFixture, async } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { TestControllerService } from './test-controller.service';
+import { WindowFocusState } from './test-controller.interfaces';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
-  beforeEach(async(() => {
+  let tcs: TestControllerService;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent]
-    }).compileComponents();
+      declarations: [AppComponent],
+      providers: [TestControllerService]
+    });
     fixture = TestBed.createComponent(AppComponent);
+    tcs = TestBed.inject(TestControllerService);
     component = fixture.componentInstance;
-  }));
+    fixture.detectChanges();
+  });
 
-  it('should create the app', async () => {
+  it('should create the app and add listeners', async () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have as title \'IQB Verona Player Testbed\'', () => {
+  it('should have and render title \'IQB Verona Player Testbed\'', () => {
     expect(component.title).toEqual('IQB Verona Player Testbed');
-  });
-
-  it('should render title', () => {
-    fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.title').textContent).toContain('IQB Verona Player Testbed');
   });
 
-  it('should create focus listeners', () => {
+  it('should create event and focus listeners', () => {
     spyOn(window, 'addEventListener');
     component.ngOnInit();
     expect(window.addEventListener).toHaveBeenCalled();
+  });
+
+  it('pass on fired window focus events', () => {
+    spyOn(tcs.windowFocusState$, 'next');
+    window.dispatchEvent(new Event('focus'));
+    expect(tcs.windowFocusState$.next).toHaveBeenCalledWith(WindowFocusState.HOST);
+    window.dispatchEvent(new Event('unload'));
+    expect(tcs.windowFocusState$.next).toHaveBeenCalledWith(WindowFocusState.UNKNOWN);
+    window.dispatchEvent(new Event('blur'));
+    expect(tcs.windowFocusState$.next).toHaveBeenCalledWith(WindowFocusState.UNKNOWN);
+  });
+
+  it('should handle MessageEvents', () => {
+    spyOn(tcs.postMessage$, 'next');
+    const testEvent: MessageEvent = new MessageEvent('message', {
+      data: {
+        type: 'vopReadyNotification',
+        apiVersion: '2'
+      }
+    });
+    window.dispatchEvent(testEvent);
+    expect(tcs.postMessage$.next).toHaveBeenCalledWith(testEvent);
+
+    const faultyTestEvent: MessageEvent = new MessageEvent('message', {
+      data: {
+        type: 'murks',
+        apiVersion: '2'
+      }
+    });
+    window.dispatchEvent(faultyTestEvent);
+    expect((tcs.postMessage$.next as jasmine.Spy).calls.count()).toBe(1);
   });
 });
