9f13b06c85a122b7833eebced7f49d70563d77cd
rhenck <richard.henck@iqb.hu-berlin.de>
2020-12-17 13:46:35 +0100

    Remove unused import

--- src/app/unit-host/unit-route-guards.ts
+++ src/app/unit-host/unit-route-guards.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot
-} from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable } from 'rxjs';
 import { TestControllerService } from '../test-controller.service';
